01237b91cc4e765bd65e5bf334f2379328e54529
Kofo Okesola <okesolakofo@gmail.com>
2019-03-24 00:41:42 +0100

    started home page

---
+++ src/components/SiteMenu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="site-menu">
+        <div class="menu-link">
+            <a>
+                About Me
+            </a>
+        </div>
+        <div class="menu-link">
+            <a>
+                Works
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SiteMenu"
+    }
+</script>
+
+<style scoped>
+
+</style>
