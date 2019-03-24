726a17b6802a766f45c9f86975a52826c1f6da3f
Kofo Okesola <okesolakofo@gmail.com>
2019-03-24 21:39:25 +0100

    wip: home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
-    <div class="content"></div>
+    <div class="content">
+      <div class="logo">
+        <img src="../assets/logo.png"/>
+      </div>
+      <div class="home-content__intro">KOFO OKESOLA</div>
+      <div class="home-content__about">A software developer and goof</div>
+      <div class="home__social">
+        <a href="https://twitter.com/yoruba_dev" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://github.com/kofoworola" target="_blank"><i class="fab fa-github"></i></a>
+      </div>
+    </div>
   </div>
 </template>
 
