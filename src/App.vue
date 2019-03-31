12809a0a7f1b380e1af59176fe50d0978bbd0922
Kofo Okesola <okesolakofo@gmail.com>
2019-03-31 02:27:58 +0100

    done animated entry

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
-  <div id="app">
-      <site-menu></site-menu>
-    <router-view/>
-  </div>
+    <div id="app">
+        <transition name="fade">
+            <site-menu v-if="route === 'home'"></site-menu>
+            <div class="top-menu" v-else>
+                <router-link to="/" class="top-menu__home">
+                    <img src="./assets/logo.png"/>
+                </router-link>
+                <router-link to="/about" class="top-menu__link" v-if="route === 'works'">
+                    About
+                </router-link>
+                <router-link to="/works" class="top-menu__link" v-if="route === 'about'">
+                    Works
+                </router-link>
+            </div>
+        </transition>
+        <router-view></router-view>
+    </div>
 </template>
 <script>
     import SiteMenu from "./components/SiteMenu";
+
     export default {
-        components: {SiteMenu}
+        components: {SiteMenu},
+        computed: {
+            route() {
+                return this.$route.name;
+            }
+        }
     }
 </script>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,10 +1,17 @@
 <template>
-    
+    <div class="works" v-on:scroll.native="scrollMethod">
+
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Works"
+        name: "Works",
+        methods: {
+            scrollMethod(){
+                console.log("Scrolling")
+            }
+        }
     }
 </script>
 
