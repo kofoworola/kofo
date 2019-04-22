57d8875dfb3a2236e8525e7aa1f8fc42f2dec2b2
Kofo Okesola <okesolakofo@gmail.com>
2019-04-22 06:00:06 +0100

    home page finessing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
             <div class="home-content__intro">KOFO OKESOLA</div>
             <div class="home-content__about" v-if="!transition">A Software Developer And</div>
             <div>
-                <p class="typing animate-on-entry">Part Time Goof</p>
+                <p class="typing animate-on-entry" v-on:animationiteration="iteration">{{ personalities[current]}}</p>
             </div>
             <div class="triangle-image">
                 <img src="../assets/backgrounds/triangle.png"/>
@@ -21,14 +21,32 @@
 </template>
 
 <script>
-    // @ is an alias to /src
-
     export default {
         name: 'home',
         components: {},
+        data() {
+            return {
+                personalities: [
+                    "part time geek",
+                    "terrible gamer",
+                    "slightly terrible cook",
+                    "aspiring vloger",
+                    "wannabe model",
+                    "more..."
+                ],
+                current:0,
+            }
+        },
         computed: {
             transition() {
                 return this.$store.state.transition;
+            }
+        },
+        methods: {
+            iteration(param) {
+                if (param.animationName === "typing") {
+                    this.current++;
+                }
             }
         }
     }
